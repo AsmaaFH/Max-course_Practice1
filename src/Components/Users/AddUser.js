ef1633279beb97efea496bfee538f0ecc7ffd3e0
Asmaa <asmaakhdr24@gmail.com>
2022-12-16 19:00:43 +0200

    Add Validation Logic

--- src/Components/Users/AddUser.js
+++ src/Components/Users/AddUser.js
@@ -4,9 +4,18 @@ import Card from '../UI/Card';
 import styles from './AddUser.module.css';
 const AddUser = (props) => {
   const [enteredUsername, setEnteredUsername] = useState('');
-  const [enteredAge, setEnteredAge] = useState(0);
-  const addUserHandler = () => {
+  const [enteredAge, setEnteredAge] = useState('');
+  const addUserHandler = (event) => {
+    event.preventDefault();
+    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+      return;
+    }
+    if (+enteredAge < 1) {
+      return;
+    }
     console.log(enteredUsername, enteredAge);
+    setEnteredUsername('');
+    setEnteredAge('');
   };
   const usernameChangeHandler = (event) => {
     setEnteredUsername(event.target.value);
